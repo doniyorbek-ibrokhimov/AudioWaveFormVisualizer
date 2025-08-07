5a9d45d78c7f5c1006a0e57b472e137d3b2e6e3e
Doniyorbek Ibrokhimov <ibrokhimovdoniyorbek@gmail.com>
2025-08-07 13:43:19 +0500

    Initial Commit

---
+++ AudioWaveFormVisualizer/AudioWaveFormVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioWaveFormVisualizerApp.swift
+//  AudioWaveFormVisualizer
+//
+//  Created by Doniyorbek Ibrokhimov on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioWaveFormVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioWaveFormVisualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioWaveFormVisualizer
+//
+//  Created by Doniyorbek Ibrokhimov on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
